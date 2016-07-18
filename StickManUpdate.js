06844ca211d4c63dc9a9ac7d83c239820a034815
the10thWiz <mpomes@wi.rr.com>
2016-07-18 16:56:52 -0500

    Update StickManUpdate.js

--- StickManUpdate.js
+++ StickManUpdate.js
@@ -1,15 +1,15 @@
 browser.browserAction.onClicked.addListener(function() {
-	browserAction.getTitle(function(t) {
+	browser.browserAction.getTitle({}, function(t) {
 		if(t === 'Call StickMan') {
-			chrome.tabs.executeScript(null, {
+			browser.tabs.executeScript(null, {
 				file: "/content_scripts/stickman.js"
 			});
-			browserAction.setTitle('Recall StickMan');
+			browser.browserAction.setTitle('Recall StickMan');
 		}else {
 			browser.tabs.sendMessage({
 				message: "End"
 			});
-			browserAction.setTitle('Call StickMan');
+			browser.browserAction.setTitle('Call StickMan');
 		}
 	})
 });
